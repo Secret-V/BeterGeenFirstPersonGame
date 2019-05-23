0fd957f51f25636ccee68d4c45915434c115a163
Vincent Booman <vincent_booman@msn.com>
2019-05-23 11:36:18 +0200

    Comments toegevoegd

--- BeterGeenFirstPersonGame/Assets/CameraMove.cs
+++ BeterGeenFirstPersonGame/Assets/CameraMove.cs
@@ -10,20 +10,21 @@ public class CameraMove : MonoBehaviour
         StartCoroutine(MoveOverField());
     }
 
+    //Coroutine, speciale functie die gepauzeerd kan worden, begint altijd met IEnumerator
     IEnumerator MoveOverField()
     {
         float timer = .0f;
 
         while(timer < .5f)
         {
-            timer += Time.deltaTime;
+            timer += Time.deltaTime;//deltaTime is de tijd tussen het vorige frame en het huidige frame
 
             transform.position -= new Vector3(5f * Time.deltaTime, .0f, .0f);
 
-            yield return 0;
+            yield return 0; //Pauzeer de coroutine en ga het volgende frame weer verder
         }
 
-        yield return new WaitForSeconds(.5f);
+        yield return new WaitForSeconds(.5f); //Pauzeer de coroutine en ga over een halve seconde weer verder
 
         while(timer < 1.5f)
         {
